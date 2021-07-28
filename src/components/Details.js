02aef7d3a395c070f4babde9d88796c951ce95c7
LuiCoro <luiacorona029@icloud.com>
2021-07-28 15:23:03 -0700

    'Thinking' About Styling Options!

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { CardSubtitle, CardText } from 'reactstrap';
+import styled from 'styled-components';
+
+// Styling
+
+const CardTitle = styled.h3`
+	color: blue;
+`;
 
 const Details = props => {
 	const { name, date, detail } = props;
+
 	return (
 		<div>
-			<h1>Name: {name}</h1>
-			<h2>{date}</h2>
-			<p>{detail}</p>
+			<CardSubtitle tag='h3'>Name: {name}</CardSubtitle>
+			<CardText color='info' tag='p'>
+				<CardTitle>{date}</CardTitle>
+				<>{detail}</>
+			</CardText>
 		</div>
 	);
 };
